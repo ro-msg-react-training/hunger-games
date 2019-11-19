c35a271cbcabb89290896f4c7bb8056725cd6f51
Ligia Nicolovici <Ligia.Nicolovici@msg.group>
2019-11-19 16:06:25 +0200

    -created login dumb component; -created one css file for styling -added one resource folder for images and gifs -implemented routing for test

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,16 @@ import { store } from "./state/store";
 import { history } from "./state/history";
 import { Home } from "./component/example/home";
 import { SmartExampleList } from "./component/example/smart-example-list";
+import { LoginDisplay } from "./component/Login/dumbLogin";
 
 const App: React.FC = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
+      <LoginDisplay {...{} as any}></LoginDisplay>
       <Switch>
         <Route exact path="/home" component={Home} />
         <Route exact path="/examples" component={SmartExampleList} />
-        <Redirect to="/home" />
+        <Route exact path="/login" component={LoginDisplay}/>
       </Switch>
     </ConnectedRouter>
   </Provider>

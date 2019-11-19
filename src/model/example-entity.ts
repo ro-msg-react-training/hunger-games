f027c80ee082c092c53010e056e84906059eec2b
Ligia Nicolovici <Ligia.Nicolovici@msg.group>
2019-11-19 09:01:48 +0200

    created data model for restaurants, food and users

--- src/model/example-entity.ts
+++
@@ -1,5 +0,0 @@
-export interface ExampleEntity {
-    id: string;
-    name: string;
-    description: string;
-}
---
+++ src/model/restaurants.ts
@@ -0,0 +1,19 @@
+export interface IRestaurant {
+    id: number;
+    restaurant_name: string;
+    adress: string;
+    open_close_interval:TimeRanges;
+    image:string;
+    no_of_orders:number;
+    food_list:IFood[];
+}
+export interface IFood{
+    id_restaurant:number;
+    food_name:string;
+    ingredients:string;
+    price:number;
+}
+export interface IUser{
+    username:string,
+    password:string
+}

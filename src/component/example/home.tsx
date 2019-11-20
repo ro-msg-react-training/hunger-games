376d98e8fae990320680ea73b5b5a39e931430e6
Andrei Astanei <Andrei.Astanei@msg.group>
2019-11-20 12:22:21 +0200

    Modificare Loading Indicator

--- src/component/example/home.tsx
+++ src/component/example/home.tsx
@@ -6,7 +6,6 @@ export const Home: React.FC = () => (
     <section className="hero is-primary is-fullheight">
         <div className="hero-body">
             <div className="container has-text-centered">
-                <PageLoader />
                 <h1 className="title">
                     Home
                 </h1>

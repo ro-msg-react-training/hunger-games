1417376eb7309360ac292336db0c9e737c887212
Ligia Nicolovici <Ligia.Nicolovici@msg.group>
2019-11-22 11:35:37 +0200

    Logout activateNavbar property changed to false

--- src/ReduxStore/LoginSection/reducer.ts
+++ src/ReduxStore/LoginSection/reducer.ts
@@ -1,44 +1,46 @@
-import * as LTypes from './types';
+import * as LTypes from "./types";
 
 export const initialState: LTypes.LoginState = {
-    userData:{
-        username:"",
-        password:""
-    },
-    isLoggedIn: false,
-    isLoading: false,
-    activateNavbar:false
+  userData: {
+    username: "",
+    password: ""
+  },
+  isLoggedIn: false,
+  isLoading: false,
+  activateNavbar: false
 };
 
-export function loginReducer(state: LTypes.LoginState = initialState, action: LTypes.LoginActionTypes):  LTypes.LoginState {
-    switch (action.type) {
-        case LTypes.LOGIN_ACTION: {
-            return {
-                userData:action.userInfo,
-                activateNavbar:true,
-                isLoading:true,
-                isLoggedIn:true
-            }
-        }
-
-        case LTypes.LOGOUT_ACTION: {
-            return {
-                userData:{} as any,
-                isLoggedIn:false,
-                isLoading:false,
-                activateNavbar:true
-
-            };
-        }
-
-        case LTypes.LOAD_PAGE: {
-            return {
-                ...state,
-                isLoading: true
-            };
-        }
-
-        default:
-            return state;
+export function loginReducer(
+  state: LTypes.LoginState = initialState,
+  action: LTypes.LoginActionTypes
+): LTypes.LoginState {
+  switch (action.type) {
+    case LTypes.LOGIN_ACTION: {
+      return {
+        userData: action.userInfo,
+        activateNavbar: true,
+        isLoading: true,
+        isLoggedIn: true
+      };
     }
+
+    case LTypes.LOGOUT_ACTION: {
+      return {
+        userData: {} as any,
+        isLoggedIn: false,
+        isLoading: false,
+        activateNavbar: false
+      };
+    }
+
+    case LTypes.LOAD_PAGE: {
+      return {
+        ...state,
+        isLoading: true
+      };
+    }
+
+    default:
+      return state;
+  }
 }

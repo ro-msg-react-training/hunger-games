7ff58d3a7af1cdb4dd275d6082dd951c7cd3737c
Andrei Astanei <Andrei.Astanei@msg.group>
2019-12-03 10:28:10 +0200

    Restaurant foods redux and user model

--- src/ReduxStore/index.tsx
+++ src/ReduxStore/index.tsx
@@ -1,13 +1,15 @@
 import { combineReducers, createStore, compose } from "redux";
+import { RestaurantDishesState } from "./RestaurantDishesSection/types";
+import { restaurantDishesReducer } from "./RestaurantDishesSection/reducers";
 
 // ex: prodListReducer : ProductListState;
 export interface GlobalState {
-
+    restDishesReducer : RestaurantDishesState
 }
 
 // ex: prodListReducer : productListReducer
 const rootReducer = combineReducers({
-
+    restDishesReducer : restaurantDishesReducer
 });
 
 // Lipseste partea de Saga

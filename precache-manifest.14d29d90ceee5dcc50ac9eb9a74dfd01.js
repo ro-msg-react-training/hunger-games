5cbc5a6d02cea8018d496b0c811c44c289da66ef
ligianicolovici <52535211+ligianicolovici@users.noreply.github.com>
2020-01-20 14:28:47 +0000

    Deploying to gh-pages from master 9927efb884d1980314142a937182db1fb18b3d53

---
+++ precache-manifest.14d29d90ceee5dcc50ac9eb9a74dfd01.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfa3ec961c1866017b0c1c75866f0c1",
+    "url": "/hunger-games/index.html"
+  },
+  {
+    "revision": "155ca78d14a0cdea213f",
+    "url": "/hunger-games/static/css/main.a34be71d.chunk.css"
+  },
+  {
+    "revision": "c085d414415b31034ba5",
+    "url": "/hunger-games/static/js/2.2313d038.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/hunger-games/static/js/2.2313d038.chunk.js.LICENSE"
+  },
+  {
+    "revision": "155ca78d14a0cdea213f",
+    "url": "/hunger-games/static/js/main.a8ca4838.chunk.js"
+  },
+  {
+    "revision": "ef58e3a2831d5f1ffa94",
+    "url": "/hunger-games/static/js/runtime-main.9344b175.js"
+  },
+  {
+    "revision": "ad47af29ad50df1477b9413f9d521db0",
+    "url": "/hunger-games/static/media/background.ad47af29.jpg"
+  },
+  {
+    "revision": "25274990f0e150ced701b54b3ed62d6b",
+    "url": "/hunger-games/static/media/bigMac1.25274990.jpg"
+  },
+  {
+    "revision": "d9bf01f967f8b8651b003428a098a1c5",
+    "url": "/hunger-games/static/media/food2.d9bf01f9.jpg"
+  },
+  {
+    "revision": "e7d1ae111981e3d829cef05ccce8bc4d",
+    "url": "/hunger-games/static/media/food3.e7d1ae11.jpg"
+  },
+  {
+    "revision": "938530190e8696e9e312cea0207f1901",
+    "url": "/hunger-games/static/media/food4.93853019.jpg"
+  },
+  {
+    "revision": "83cf2da1ed25c747777329dc67805d89",
+    "url": "/hunger-games/static/media/logo.83cf2da1.png"
+  },
+  {
+    "revision": "7f1d3fc9145f6c5fc0fed276e0703cab",
+    "url": "/hunger-games/static/media/no_image_found.7f1d3fc9.png"
+  },
+  {
+    "revision": "29483d95a01e53e376939bae351c2bbb",
+    "url": "/hunger-games/static/media/pizza1.29483d95.jpg"
+  },
+  {
+    "revision": "e5e568ab84324746a1848e30fe048758",
+    "url": "/hunger-games/static/media/salad1.e5e568ab.jpg"
+  }
+]);
